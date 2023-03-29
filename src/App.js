2ff2f26d5bb1f0d8a10f77f8c9cbe988e68bb7fa
Andee Warren <119145298+andeewarren@users.noreply.github.com>
2023-03-29 14:43:09 -0400

    updates for deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
-import SubmitButton from './components/SubmitButton';
 
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
           <>
               <Navbar />
               <LoginForm />
-
           </>
       );
 }
